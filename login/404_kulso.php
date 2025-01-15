552faf191ef787cafc1ebdabb556aad1ca81c473
JankoPetra <91984558+JankoPetra@users.noreply.github.com>
2025-01-15 12:42:35 +0100

    Add files via upload

---
+++ login/404_kulso.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body {
+            height: 100vh;
+            background: url(background.jpg) no-repeat;
+            background-size: cover;
+            background-position: center;
+        }
+
+        h1 {
+            font-size: 3rem;
+            margin-bottom: 1rem;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Neked holnap már nem fütyül a rigó</h1>
+</body>
+</html>
