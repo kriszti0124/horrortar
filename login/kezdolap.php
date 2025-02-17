091be2ca26e3b28ac3b8f01ce04cc4ad1c9bb401
JankoPetra <91984558+JankoPetra@users.noreply.github.com>
2025-02-17 13:19:57 +0100

    Update kezdolap.php

--- login/kezdolap.php
+++ login/kezdolap.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <title>Horrortár</title>
+</head>
+
 <style>
     * {
         margin: 0;
@@ -11,6 +20,8 @@
         background-size: cover;
         background-position: center;
     }
+
+    
 
     section#hero {
         height: calc(100vh - 60px);
@@ -26,10 +37,35 @@
         font-size: 3rem;
         margin-bottom: 1rem;
     }
+
+    @media(max-width: 992px) {
+        .navbar .links,
+        .navbar .action_btn {
+            display: none;
+        }
+
+        .navbar .toggle_btn {
+            display: block;
+        }
+
+        .dropdown_menu {
+            display: block;
+        }
+    }
+
+    @media(max-width: 576px) {
+        .dropdown_menu {
+            left: 2rem;
+            width: unset;
+        }
+    }
     
 </style>
 
 <body>
+   <?php
+   include("navigacio.php");
+   ?>
 
     <main>
         <section id="hero">
@@ -37,4 +73,19 @@
         </section>
     </main>
 
+    <script>
+        const toggleBtn = document.querySelector('.toggle_btn')
+        const toggleBtnIcon = document.querySelector('.toggle_btn i')
+        const dropdownMenu = document.querySelector('.dropdown_menu')
+
+        toggleBtn.onclick = function () {
+            dropdownMenu.classList.toggle('open')
+            const isOpen = dropdownMenu.classList.contains('open')
+
+            toggleBtnIcon.classList = isOpen
+            ? 'bx bx-x'
+            : 'bx bx-menu'
+        }
+    </script>
 </body>
+</html>
