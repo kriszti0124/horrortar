411c6dfde9968728f4de23063a88335b8e31c326
JankoPetra <91984558+JankoPetra@users.noreply.github.com>
2025-02-27 11:23:07 +0100

    Update kezdolap.php

--- login/kezdolap.php
+++ login/kezdolap.php
@@ -1,5 +1,3 @@
-<!DOCTYPE html>
-<html lang="hu">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -85,4 +83,3 @@
         }
     </script>
 </body>
-</html>
