eb864a84b069a01c9bcf01f204fd07796d7ed0eb
JankoPetra <91984558+JankoPetra@users.noreply.github.com>
2025-02-27 11:19:36 +0100

    Update reg_ir.php

--- login/reg_ir.php
+++ login/reg_ir.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     print_r($_POST);
     if($_POST['unick']=="")
     die("<script> alert('Nem adtad meg a felhasználónevet!')</script>");
@@ -20,8 +21,11 @@
         INSERT INTO user (uid, umail, unick, upw, uszuldatum, udatum, uip, usession, ustatusz, ukomment) 
         VALUES           (NULL, '$_POST[umail]', '$_POST[unick]', '$upw', '$_POST[uszuldatum]', NOW(), '', '', '', '')
     ");
-
-    die($_POST['upw']);
+    
+    if(!empty($_POST['unick']) && !empty($_POST['umail']) && !empty($_POST['uszuldatum']) && !empty($_POST['upw']) ) {
+         die("<script>alert('Sikeres regisztráció!')</script>");
+    };
+    
     mysqli_close($adb);
 
 ?>
