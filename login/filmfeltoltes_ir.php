d70093e9ac2825c8cc7b1d7496d9810de3381400
JankoPetra <91984558+JankoPetra@users.noreply.github.com>
2025-04-07 10:53:01 +0200

    Update filmfeltoltes_ir.php

--- login/filmfeltoltes_ir.php
+++ login/filmfeltoltes_ir.php
@@ -2,7 +2,7 @@
     session_start();
     include("kapcsolat.php");
 
-    
+    // Ellenőrizzük, hogy a felhasználó be van-e jelentkezve
     if (!isset($_SESSION['uid'])) {
         header('Location: ?p=login'); 
         exit();
@@ -10,39 +10,48 @@
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         
-        $fcim = mysqli_real_escape_string($adb, $_POST['fcim']);
-        $fmegjelenes = mysqli_real_escape_string($adb, $_POST['fmegjelenes']);
-        $fmufaj = mysqli_real_escape_string($adb, $_POST['fmufaj']);
-        $fleiras = mysqli_real_escape_string($adb, $_POST['fleiras']);
+        // Felhasználói bemenetek tisztítása
+        $knev = mysqli_real_escape_string($adb, $_POST['knev']);
+        $kleiras = mysqli_real_escape_string($adb, $_POST['kleiras']);
         
-        
-        $fposzter = $_FILES['fposzter']['name'];
-        $fposzter_tmp = $_FILES['fposzter']['tmp_name'];
+        // Kép feltöltés
+        $karakter_kep = $_FILES['karakter_kep']['name'];
+        $karakter_kep_tmp = $_FILES['karakter_kep']['tmp_name'];
         $upload_dir = 'uploads/'; 
         
+        // Ellenőrizzük a fájl hibakódját
+        if ($_FILES['karakter_kep']['error'] !== UPLOAD_ERR_OK) {
+            echo "<script>alert('Hiba történt a fájl feltöltése során: " . $_FILES['karakter_kep']['error'] . "');</script>";
+            exit();
+        }
         
-        $unique_filename = time() . "_" . basename($fposzter);
+        // Fájlméret ellenőrzés (max 2MB)
+        if ($_FILES['karakter_kep']['size'] > 2 * 1024 * 1024) { // 2MB
+            echo "<script>alert('A fájl túl nagy! Max 2MB lehet.');</script>";
+            exit();
+        }
+        
+        // Egyedi fájlnév generálása az ütközések elkerülése érdekében
+        $unique_filename = time() . "_" . basename($karakter_kep);
         $upload_path = $upload_dir . $unique_filename;
 
-        
-        if (move_uploaded_file($fposzter_tmp, $upload_path)) {
+        // Fájl mozgatása a szerverre
+        if (move_uploaded_file($karakter_kep_tmp, $upload_path)) {
             
-            $uid = $_SESSION['uid']; 
-
-            
+            // SQL lekérdezés a karakter adatainak mentésére
             $query = "
-                INSERT INTO filmek (fcim, fmegjelenes, fmufaj, fleiras, fposzter, ffeltoltdatum, uid) 
-                VALUES ('$fcim', '$fmegjelenes', '$fmufaj', '$fleiras', '$unique_filename', NOW(), '$uid')
+                INSERT INTO karakterek (knev, kleiras, karakter_kep) 
+                VALUES ('$knev', '$kleiras', '$unique_filename')
             ";
 
-            
+            // Lekérdezés végrehajtása
             if (mysqli_query($adb, $query)) {
-                echo "<script>alert('Film sikeresen feltöltve!'); window.location.href = './?p=belsolap';</script>";
+                echo "<script>alert('Karakter sikeresen feltöltve!'); window.location.href = './?p=belsolap';</script>";
             } else {
-                echo "<script>alert('Hiba történt a film feltöltése során: " . mysqli_error($adb) . "');</script>";
+                echo "<script>alert('Hiba történt a karakter feltöltése során: " . mysqli_error($adb) . "');</script>";
             }
         } else {
-            echo "<script>alert('Hiba történt a poszter feltöltése során!');</script>";
+            echo "<script>alert('Hiba történt a karakter kép feltöltése során!');</script>";
         }
     }
 
