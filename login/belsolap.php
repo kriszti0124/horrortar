6c98f4aeb58f1272351e29867981f81357db190c
JankoPetra <91984558+JankoPetra@users.noreply.github.com>
2025-04-10 22:52:06 +0200

    Update belsolap.php

--- login/belsolap.php
+++ login/belsolap.php
@@ -82,19 +82,80 @@
         justify-content: center;
     }
 
-    section#hero {
-        height: calc(100vh - 60px);
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        text-align: center;
-        color: #fff;
-    }
+    /*section#hero {*/
+    /*    height: calc(100vh - 60px);*/
+    /*    display: flex;*/
+    /*    flex-direction: column;*/
+    /*    align-items: center;*/
+    /*    justify-content: center;*/
+    /*    text-align: center;*/
+    /*    color: #fff;*/
+    /*}*/
 
-    #hero h1 {
-        font-size: 3rem;
-        margin-bottom: 1rem;
+    /*#hero h1 {*/
+    /*    font-size: 3rem;*/
+    /*    margin-bottom: 1rem;*/
+    /*}*/
+    
+    .about{
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center
+    }
+        
+    .about .content img {
+        height: 450px;
+        width: 450px;
+        max-width: 100%;
+        border-radius: 50%;
+    }
+        
+    .text {
+        width: 550px;
+        max-width: 100%;
+        padding: 0 10px;
+    }
+        
+    .content {
+        width: 1280px;
+        max-width: 95%;
+        margin: 0 auto;
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: space-around;
+    }
+        
+    .text h2 {
+        color: #fff;
+        font-size: 75px;
+        margin-bottom: 20px;
+        text-transform: capitalize;
+    }
+        
+    .text h5 {
+        color: #fff;
+        font-size: 25px;
+        margin-bottom: 25px;
+        letter-spacing: 2px;
+    }
+        
+    .text p {
+        color: #070A11;
+        font-size: 18px;
+        line-height: 28px;
+        letter-spacing: 1px;
+        margin-bottom: 45px;
+    }
+    
+    @media screen and (max-width: 1180px) {
+        .about {
+            width: 100%;
+            height: auto;
+            padding: 70px 0px;
+        }
     }
 
     @media(max-width: 992px) {
@@ -110,6 +171,15 @@
         .dropdown_menu {
             display: block;
         }
+        
+        .about .content img {
+            margin-bottom: 35px;
+        }
+            
+        .text h1 {
+            font-size: 60px;
+            margin-bottom: 25px;
+        }
     }
 
     @media(max-width: 576px) {
@@ -117,17 +187,43 @@
             left: 2rem;
             width: unset;
         }
+        
+        .about .content img {
+            height: 300px;
+            width: 300px;
+            margin-bottom: 35px;
+        }
+            
+        .text h1 {
+            font-size: 60px;
+            margin-bottom: 25px;
+        }
     }
     
 </style>
 
 <body>
 
-    <main>
-        <section id="hero">
-            <h1>Horrortár</h1>
-        </section>
-    </main>
+    <!--<main>-->
+    <!--    <section id="hero">-->
+    <!--        <h1>Horrortár</h1>-->
+    <!--    </section>-->
+    <!--</main>-->
+    
+    <section class="about">
+        <div class="content">
+            <img src="logo.jpeg">
+            <div class="text">
+                <h2>Rólunk</h2>
+                <h5>Horrortár</h5>
+                <p>Üdvözlünk a horrortar.hu-n, ahol a rettegés és a borzongás világába vezetjük el a horrorrajongókat! Oldalunk célja, hogy minden olyan személy számára izgalmas és hasznos tartalmat kínáljunk, aki szenvedélyesen követi a horror műfaját. Legyen szó klasszikusokról vagy az újabb, a műfajt tovább formáló alkotásokról – mi minden részletet feltárunk.
+
+                    Itt megtalálhatók filmek, kritikák, elemzések, amelyek segítenek eligazodni a horrorfilmek rengetegében. A szívünk a feszült hangulatú thrillerektől a vérgőzös slasher-ekig mindent magába foglal, amire egy igazi rajongónak szüksége lehet. Fedezd fel velünk a legújabb megjelenéseket, a kultikus klasszikusokat és a rejtett gyöngyszemeket, miközben átélheted a filmek borzongató világát.
+
+                </p>
+            </div>
+        </div>
+    </section>
 
 </body>
 
