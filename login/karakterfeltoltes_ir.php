af4ccca7db18cd84070d5018ea5db1e0c3d49495
JankoPetra <91984558+JankoPetra@users.noreply.github.com>
2025-04-10 23:12:38 +0200

    Create karakterfeltoltes_ir.php

---
+++ login/karakterfeltoltes_ir.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+// Hibák megjelenítése
+ini_set('display_errors', 1); 
+error_reporting(E_ALL);
+
+
+include("kapcsolat.php");
+
+// Ha nincs bejelentkezve a felhasználó, irányítsuk át a login oldalra
+if (!isset($_SESSION['uid'])) {
+    header('Location: ?p=login');
+    exit();
+}
+
+// Ha POST kérést kapunk
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // A karakter adatai
+    $fid = mysqli_real_escape_string($adb, $_POST['fid']);
+    $knev = mysqli_real_escape_string($adb, $_POST['knev']);
+    $kleiras = mysqli_real_escape_string($adb, $_POST['kleiras']);
+    
+    // A karakter képe
+    $karakter_kep = $_FILES['karakter_kep']['name'];
+    $karakter_kep_tmp = $_FILES['karakter_kep']['tmp_name'];
+    $upload_dir = 'uploads/';  // A fájlok feltöltésének könyvtára
+    
+    // Egyedi fájlnév generálása
+    $unique_filename = time() . "_" . basename($karakter_kep);
+    $upload_path = $upload_dir . $unique_filename;
+
+    // Ha sikerül feltölteni a képet
+    if (move_uploaded_file($karakter_kep_tmp, $upload_path)) {
+        
+        $uid = $_SESSION['uid']; // A bejelentkezett felhasználó ID-ja
+
+        // Adatbázisba való mentés
+        $query = "
+            INSERT INTO karakterek (kid, fid, knev, kleiras, karakter_kep) 
+            VALUES (NULL, '$fid', '$knev', '$kleiras', '$unique_filename')
+        ";
+
+        // Ha sikeres az adatbázisba való beszúrás
+        if (mysqli_query($adb, $query)) {
+            echo "<script>alert('Karakter sikeresen feltöltve!');</script>";
+        } else {
+            echo "<script>alert('Hiba történt a karakter feltöltése során: " . mysqli_error($adb) . "');</script>";
+        }
+    } else {
+        echo "<script>alert('Hiba történt a karakter kép feltöltése során!');</script>";
+    }
+}
+
+// Kapcsolat bezárása
+mysqli_close($adb);
+?>
